93eefefb4c4c9fda7984f30bf589eb17e4a7a83b
bk-git-hub <bkprivate99@gmail.com>
2025-11-12 11:34:12 +0900

    feat: add header component to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Header from '../component/layouts/header';
+
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+    <div className='flex flex-col min-h-screen items-center justify-center  bg-zinc-50 font-sans dark:bg-black'>
+      <Header />
       <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
         HomePage
       </main>
